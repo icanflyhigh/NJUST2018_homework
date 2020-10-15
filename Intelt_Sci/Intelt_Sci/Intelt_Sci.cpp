eeb0b05b29483bb9bf70b8b57edbe2e149505502
icanflyhigh <58471447+icanflyhigh@users.noreply.github.com>
2020-10-15 08:26:20 +0800

    final version
    增加了LGN（局部对比度归一化）（有效果，但是总体依旧不行

--- Intelt_Sci/Intelt_Sci/Intelt_Sci.cpp
+++ Intelt_Sci/Intelt_Sci/Intelt_Sci.cpp
@@ -1,4 +1,4 @@
-﻿#include <opencv2\opencv.hpp>
+#include <opencv2\opencv.hpp>
 #include <iostream>
 #include <math.h>
 #include <string>
@@ -8,6 +8,10 @@
 
 using namespace cv;
 using namespace std;
+
+String file_path = "D:/pic/";
+String file_name = "6.jpg";
+
 
 const float EPS = 1e-6;
 Mat load_pic(String & input_path)
@@ -23,8 +27,7 @@ Mat load_pic(String & input_path)
 
 
 
-String file_path = "D:/pic/";
-String file_name = "1.png";
+
 String out_img_name = file_name + "_processed";
 Mat src = load_pic(file_path += file_name), dst;
 Mat  Lz, GM, GD;
@@ -33,7 +36,7 @@ const float pi_1 = 1.0/3.1415926535;
 int mouse_time = 0;
 int mousex = -1, mousey = -1;
 float *** costmap;
-float weight1 = 0.4, weight2 = 0.4, weight3 = 0.2;
+float weight1 = 0.53, weight2 = 0.23, weight3 = 0.24;
 float sqrt2 = 1.414;
 float len_val[8] = { sqrt2, 1, sqrt2, 1, 1, sqrt2, 1, sqrt2 };
 int direct[8][2] = { {-1, -1}, {0, -1}, {1, -1}, {-1, 0}, {1, 0}, {-1, 1}, {0, 1}, {1, 1} };
@@ -63,7 +66,7 @@ struct vertex
 	}
 };
 
-priority_queue<vertex> que;
+
 float*** get_mat() 
 {
 	float *** ret = new float ** [row];
@@ -83,9 +86,52 @@ float*** get_mat()
 void getLz() 
 {
 	Mat kernel_laplacian = (Mat_<char>(3, 3) << 0, 1, 0, 1, -4, 1, 0, 1, 0);
+	Mat mean, std;
+	//Mat t1 = Mat(src);
+	Mat t1;
+	Rect rect;
+	src.convertTo(t1, CV_32FC1, 1 / 255.0);
+	float fact_std = 1;
+	int height = 2, width = 2;
+	Mat t2;
+	for (int i = 0; i < row ; i += 3)
+	{
+		width = 2;
+		if (i + 1 >= row)
+		{
+			height = row - i ;
+		}
+		for (int j = 0; j < col ; j += 3)
+		{
+			
+			if (j + 1 >= col)
+			{
+				width = col - j ;
+			}
+			t2 = t1(Rect(j, i, width, height));
+			meanStdDev(t2, mean, std);
+			for (int u = 0; u < height; u++)
+			{
+				for (int v = 0; v < width; v++)
+				{
+					for (int k = 0; k < 3; k++)
+					{
+						t1.at<Vec3f>(i + u, j + v)[k] -= mean.at<double>(0, 0);
+						t1.at<Vec3f>(i + u, j + v)[k] /= fact_std * sqrt(std.at<double>(0, 0) + EPS);
+						//t1.at<Vec3f>(i + u, j + v)[k] /= fact_std * (std.at<double>(0, 0) + EPS);
+					}
+				}
+			}
+
+
+		}
+	}
 	Mat temp;
-	filter2D(src, temp, src.depth(), kernel_laplacian);
-	temp.convertTo(Lz, CV_32FC1, 1 / 255.0);
+	filter2D(t1, Lz, t1.depth(), kernel_laplacian);
+	// temp.convertTo(Lz, CV_32FC1, 1 / 255.0);
+	
+	
+
 	for (int i = 0; i < row; i++)
 	{
 		for (int j = 0; j < col; j++)
@@ -102,9 +148,11 @@ void getLz()
 				}
 			}
 			// 通过0号通道纪律均值
-			Lz.at<Vec3f>(i, j)[0] = (Lz.at<Vec3f>(i, j)[0]+Lz.at<Vec3f>(i, j)[1]+ Lz.at<Vec3f>(i, j)[2])/3;
+			// Lz.at<Vec3f>(i, j)[0] = (Lz.at<Vec3f>(i, j)[0]+Lz.at<Vec3f>(i, j)[1]+ Lz.at<Vec3f>(i, j)[2])/3;
+			Lz.at<Vec3f>(i, j)[0] = max(Lz.at<Vec3f>(i, j)[0],max(Lz.at<Vec3f>(i, j)[1], Lz.at<Vec3f>(i, j)[2]));
 		}
 	}
+
 
 }
 
@@ -148,8 +196,9 @@ void getGM_GD(Mat & GM, Mat & GD)
 				if (maxx < Ix.at<Vec3f>(i, j)[k]) maxx = Ix.at<Vec3f>(i, j)[k];
 				if (maxy < Iy.at<Vec3f>(i, j)[k]) maxy = Iy.at<Vec3f>(i, j)[k];
 			}
-			// 通过0号通道记录最大值
-			GM.at<Vec3f>(i, j)[0] = (GM.at<Vec3f>(i, j)[0]+GM.at<Vec3f>(i, j)[1]+ GM.at<Vec3f>(i, j)[2])/3;
+			// 通过0号通道记录
+			//GM.at<Vec3f>(i, j)[0] = (GM.at<Vec3f>(i, j)[0]+GM.at<Vec3f>(i, j)[1]+ GM.at<Vec3f>(i, j)[2])/3;
+			GM.at<Vec3f>(i, j)[0] = max(GM.at<Vec3f>(i, j)[0], max(GM.at<Vec3f>(i, j)[1], GM.at<Vec3f>(i, j)[2]));
 			// 取最大的梯度通道
 			Ix.at<Vec3f>(i, j)[1] = sqrt(maxx*maxx + maxy * maxy);
 			if (Ix.at<Vec3f>(i, j)[1] < 0.0001)
@@ -198,11 +247,16 @@ void getGM_GD(Mat & GM, Mat & GD)
 	
 }
 
-void find_path(int sx, int sy, int dx, int dy)
+
+
+
+priority_queue<vertex> que;
+int sx, sy;
+bool ** vis = new bool *[row];
+int ** fax = new int *[row], **fay = new int *[row];
+void ini()
 {
-	que.push(vertex(sx, sy, -1, -1, 0.0));
-	bool ** vis = new bool *[row];
-	int ** fax = new int * [row], **fay = new int *[row];
+	mousex = -1, mousey = -1;
 	for (int i = 0; i < row; i++)
 	{
 		vis[i] = new bool[col];
@@ -213,7 +267,13 @@ void find_path(int sx, int sy, int dx, int dy)
 			vis[i][j] = false;
 		}
 	}
-
+}
+bool compute_flag = false;
+void find_path(int dx, int dy)
+{
+	
+	if (vis[dx][dy]) return;
+	compute_flag = true;
 	while (!que.empty())
 	{
 		vertex tv = que.top();
@@ -231,49 +291,61 @@ void find_path(int sx, int sy, int dx, int dy)
 			int ty = tv.y + direct[i][1];
 			if (tx < 0 || tx >= row || ty < 0 || ty >= col||vis[tx][ty])
 				continue;
-			que.push(vertex(tx, ty, tv.x, tv.y, tv.cost+costmap[tv.x][tv.y][i]+ straight_fact * (abs(tx-dx)+abs(tx-dy))));
+			//que.push(vertex(tx, ty, tv.x, tv.y, tv.cost+costmap[tv.x][tv.y][i]+ straight_fact * (abs(tx-dx)+abs(tx-dy))));
+			que.push(vertex(tx, ty, tv.x, tv.y, tv.cost+costmap[tv.x][tv.y][i]));
 		}
 	}
-	int tx=dx, ty=dy;
-	while (fax[tx][ty] != -1)
-	{
-		//cout << tx << "  " << ty << endl;
-		dst.at<Vec3b>(tx, ty)[0] = (unsigned char)0;
-		dst.at<Vec3b>(tx, ty)[1] = (unsigned char)255;
-		dst.at<Vec3b>(tx, ty)[2] = (unsigned char)0;
-		int tempx = fax[tx][ty], tempy = fay[tx][ty];
-		tx = tempx, ty = tempy;
-	}
-	for (int i = 0; i < row; i++)
-	{
-		delete vis[i];
-		delete fax[i] ;
-		delete fay[i] ;
-	}
-	delete vis;
-	delete fax;
-	delete fay;
-	while (!que.empty())que.pop();
+	compute_flag = false;
 }
 
+void draw_path(int x, int y)
+{
+	compute_flag = true;
+	while (fax[x][y] != sx || fay[x][y] != sy)
+	{
+		int tx = fax[x][y];
+		int ty = fay[x][y];
+		dst.at<Vec3b>(x, y)[0] = 0;
+		dst.at<Vec3b>(x, y)[1] = 255;
+		dst.at<Vec3b>(x, y)[2] = 0;
+		x = tx, y = ty;
+	}
+	//cout << sx << "  " << sy << endl;
+	//cout << x << "  " << y << endl;
+	imshow(out_img_name, dst);
+	compute_flag = false;
+}
 void onMouse(int event, int x, int y, int flags, void* param)
 {
-	if (event == 1)
+	if (event == 1 && !compute_flag)
 	{
-		cout << x << "  " << y << endl;
+		cout << "event1" << x << "  " << y << endl;
 		if (mouse_time == 1)
 		{
-			find_path(mousey, mousex, y, x);
-			imshow(out_img_name, dst);
+			draw_path(y, x);
+			for (int i = 0; i < row; i++)
+			{
+				for (int j = 0; j < col; j++)
+				{
+					vis[i][j] = false;
+					fax[i][j] = -1;
+					fay[i][j] = -1;
+				}
+			}
+			while (!que.empty())
+			{
+				que.pop();
+			}
 		}
+		sx = y, sy = x;
 		mousex = x, mousey = y;
 		mouse_time = 1;
-		
-		cout << "event1" << endl;
+		que.push(vertex(y, x, y, x, 0.0));
 	}
-	if (event == 0 && mousex != -1)
+	if (event == 0 && mousex != -1&& !( y == sx && x == sy ) &&!compute_flag)
 	{
-		//find_path(mousey, mousex, y, x);
+		//cout << "event0" <<y<<"  "<<x<< endl;
+		find_path(y, x);
 		
 	}
 }
@@ -284,12 +356,13 @@ int main()
 
 	//std::cout << ("Please Input the file Name:");
 	//std::cin >> file_name;
-	
+	printf("请在源文件中修改读取图片路径\n");
 
 	if (src.empty())
 	{
 		return 0;
 	}
+	ini();
 	costmap = get_mat();
 	getLz();
 	getGM_GD(GM, GD);
@@ -298,5 +371,6 @@ int main()
 	imshow(out_img_name, src);
 	setMouseCallback(out_img_name, onMouse);
 	waitKey();
+	imwrite(file_path + out_img_name, dst);
 	return 0;
 }
